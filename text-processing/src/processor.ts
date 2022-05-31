e223c84f7b30e8006aa764d87fbe3003a2991b06
José Manuel Serrano Mármol <jmserrano.dev@outlook.com>
2022-05-31 22:14:52 +0200

    🔵: Extract generation dictionary and complex types

--- text-processing/src/processor.ts
+++ text-processing/src/processor.ts
@@ -1,31 +1,38 @@
+type Word = string;
+
+type WordDictionary = { [key: Word]: number };
+
 export class Processor {
-  private distinctWords: { [key: string]: number } = {};
   private totalWords: number = 0;
+  private dictionary: WordDictionary = {};
 
   public analyse(text: string) {
     const words = this.splitIntoWords(text);
+    const dictionary = this.getWordDictionary(words);
 
-    const distinctWords = words.reduce((previous, current) => {
-      previous[current] = (previous[current] || 0) + 1;
-      return previous;
-    }, {});
-
+    this.dictionary = dictionary;
     this.totalWords = words.length;
-    this.distinctWords = distinctWords;
   }
 
   public getTop10() {
-    return Object.entries(this.distinctWords)
+    return Object.entries(this.dictionary)
       .sort((x, y) => y[1] - x[1])
       .slice(0, 10)
-      .map((entry) => entry[0]);
+      .map(([word]) => word);
   }
 
   public getTotalWords() {
     return this.totalWords;
   }
 
-  private splitIntoWords(text: string) {
+  private splitIntoWords(text: string): Word[] {
     return text.split(" ").filter(Boolean);
+  }
+
+  private getWordDictionary(words: Word[]): WordDictionary {
+    return words.reduce((previous, current) => {
+      previous[current] = (previous[current] || 0) + 1;
+      return previous;
+    }, {});
   }
 }
